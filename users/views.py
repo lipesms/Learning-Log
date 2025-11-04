424a90c637b6bdab85444aacfa945b5d74543bcd
lipesms <femartins2010@gmail.com>
2025-11-03 23:15:39 -0300

    Finalizando a estilização

--- users/views.py
+++ users/views.py
@@ -11,19 +11,23 @@ def logout_view(request):
 
 def register(request):
     """Registra um novo usuário."""
-    if request.method != 'POST':
-        #Exibe o formulário do cadastro em branco
-        form = UserCreationForm()
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('index'))
     else:
-        #Processa o formulário preenchido
-        form = UserCreationForm(data=request.POST)
-        
-        if form.is_valid():
-            new_user = form.save()
-            #Faz login do usuário e o redireciona para a página inicial
-            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
-            login(request, authenticated_user)
-            return HttpResponseRedirect(reverse('index'))
+        if request.method != 'POST':
+            #Exibe o formulário do cadastro em branco
+            form = UserCreationForm()
+        else:
+            #Processa o formulário preenchido
+            form = UserCreationForm(data=request.POST)
+            
+            if form.is_valid():
+                new_user = form.save()
+                #Faz login do usuário e o redireciona para a página inicial
+                authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+                login(request, authenticated_user)
+                return HttpResponseRedirect(reverse('index'))
 
     context = {'form': form}
     return render(request, 'users/register.html', context)
